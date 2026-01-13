8ba9fb853638d38196c372b94d83192809c0ec18
Assaf <assafyekuel@gmail.com>
2026-01-12 22:57:06 -0800

    Increase PWA max file size cache limit to 10MB

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         runtimeCaching: [
           {
